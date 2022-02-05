18b555deb1e4fe367b8735a4249c3e75c59304bf
tecniphone <quim.tecniphone@gmail.pt>
2022-02-05 17:11:44 +0000

    comite primeira alteracao

---
+++ teste2guit/src/teste2guit/Teste2guit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste2guit;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class Teste2guit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+    
+}
